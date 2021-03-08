cd0dc5ed3fe6f1eee420099d911e78c9a9a6e067
hennylee <haenyilee@kakao.com>
2021-03-08 16:49:17 +0900

    day1 github connect

--- _code/2021-03-08-java-basic-01/src/kr/ac/kopo/day01/ConstMain.java
+++ _code/2021-03-08-java-basic-01/src/kr/ac/kopo/day01/ConstMain.java
@@ -5,5 +5,6 @@ public class ConstMain {
 	public static void main(String[] args) {
 		System.out.printf("%c%c\n", 'A', 'b'); //Ab
 		System.out.println('a' + 10 + 'B' + "A"); //173A
+		System.out.println("git 연동 완료");
 	}
 }
