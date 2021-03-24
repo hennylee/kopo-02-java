9677534d139ec192f03602dce51acbd994b1a47c
hennylee <haenyilee@kakao.com>
2021-03-24 15:51:45 +0900

    0324 숙제 문제

---
+++ _code/java-basic/src/kr/ac/kopo/day13/homework/CalendarMain.java
@@ -0,0 +1,9 @@
+package kr.ac.kopo.day13.homework;
+
+public class CalendarMain {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ _code/java-basic/src/kr/ac/kopo/day13/homework/LottoExceptionMain.java
@@ -0,0 +1,10 @@
+package kr.ac.kopo.day13.homework;
+
+public class LottoExceptionMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
